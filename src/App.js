e8edd55937f1e1ba46491a9bd109ad3f23563643
james <james@irm.kr>
2025-01-28 00:17:23 +0900

    fix: travis.yml file added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const count = 0;
+
   return (
     <div className="App">
       <header className="App-header">
@@ -15,7 +18,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          안녕, 반가워~!
+          안녕, 파일 수정했어.
+          {count}
         </a>
       </header>
     </div>
